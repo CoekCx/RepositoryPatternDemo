54292bc1e614627e5f9e36352bb2c8e022b2bbb4
CoekCx <nemanja.ja.jovicic7@gmail.com>
2024-10-28 10:52:45 +0100

    added AppDbContext.cs

---
+++ UnitOfWorkPatternDemo/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitOfWorkPatternDemo.Models;
+
+namespace UnitOfWorkPatternDemo.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+}
