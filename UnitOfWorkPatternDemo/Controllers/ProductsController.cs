c0a70e7566d26a136a3d4c4a01801f59050942ee
CoekCx <nemanja.ja.jovicic7@gmail.com>
2024-10-28 11:08:04 +0100

    added products controller to use the unit of work

---
+++ UnitOfWorkPatternDemo/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnitOfWorkPatternDemo.Models;
+using UnitOfWorkPatternDemo.Repositories.Abstractions;
+
+namespace UnitOfWorkPatternDemo.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ProductsController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts()
+    {
+        var products = await _unitOfWork.Products.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddProduct(Product product)
+    {
+        await _unitOfWork.Products.AddAsync(product);
+        await _unitOfWork.CompleteAsync();
+        return Ok(product);
+    }
+}
--- UnitOfWorkPatternDemo/Program.cs
+++ UnitOfWorkPatternDemo/Program.cs
@@ -5,13 +5,36 @@ using UnitOfWorkPatternDemo.Repositories.Implementations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure PostgreSQL
+// Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Authorization services
+builder.Services.AddAuthorization();
+
+// Add Controllers services
+builder.Services.AddControllers();
+
+// Add Swagger/OpenAPI in one line
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Register Unit of Work
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // Use Swagger with one line
+    app.UseSwagger(); // Serves Swagger at /swagger/v1/swagger.json
+    app.UseSwaggerUI(); // Serves the Swagger UI at /swagger
+}
+
+app.UseAuthorization();
+
+// Map Controllers
 app.MapControllers();
+
 app.Run();
