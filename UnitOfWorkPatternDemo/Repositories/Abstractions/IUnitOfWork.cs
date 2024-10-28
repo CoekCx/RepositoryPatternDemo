5b0ce8b45e742bbcc5b8552f37a14bdaeac94077
CoekCx <nemanja.ja.jovicic7@gmail.com>
2024-10-28 10:57:53 +0100

    implement the unit of work

---
+++ UnitOfWorkPatternDemo/Repositories/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using UnitOfWorkPatternDemo.Models;
+
+namespace UnitOfWorkPatternDemo.Repositories.Abstractions;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<Product> Products { get; }
+    IRepository<Order> Orders { get; }
+    Task<int> CompleteAsync();
+}
---
+++ UnitOfWorkPatternDemo/Repositories/Implementations/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using UnitOfWorkPatternDemo.Data;
+using UnitOfWorkPatternDemo.Models;
+using UnitOfWorkPatternDemo.Repositories.Abstractions;
+
+namespace UnitOfWorkPatternDemo.Repositories.Implementations;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+        Products = new Repository<Product>(_context);
+        Orders = new Repository<Order>(_context);
+    }
+
+    public IRepository<Product> Products { get; private set; }
+    public IRepository<Order> Orders { get; private set; }
+
+    public async Task<int> CompleteAsync() => await _context.SaveChangesAsync();
+
+    public void Dispose() => _context.Dispose();
+}
